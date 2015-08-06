5544cb23500c5bbdd845c8165e895f492b51a55f
Adam Ki Jensen <adajensen@gmail.com>
2015-08-05 18:37:28 -0600

    spacing and underscores added

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -4,40 +4,28 @@ require_relative 'customer_repo'
 
 class SalesEngine
 
-  attr_accessor :customer_repo,
-                :customer_data,
-                :merchant_data,
-                :invoice_data,
-                :item_data,
-                :invoiceitem_data,
-                :transaction_data,
-                :customer_repo,
-                :merchant_repo,
-                :invoice_repo,
-                :item_repo,
-                :invoiceitem_repo,
-                :transaction_repo
-
-
-
+  attr_accessor :customer_data,    :customer_repo,
+                :merchant_data,    :merchant_repo,
+                :invoice_data,     :invoice_repo,
+                :item_data,        :item_repo,
+                :invoice_item_data,:invoice_item_repo,
+                :transaction_data, :transaction_repo
 
   def initialize
-    @customer_data = CustomerLoader.new.load_all
-    @merchant_data = MerchantLoader.new.load_all
-    @invoice_data  = InvoiceLoader.new.load_all
-    @item_data     =     ItemLoader.new.load_all
-    @invoiceitem_data =     InvoiceItemLoader.new.load_all
-    @transaction_data = TransactionLoader.new.load_all
-
-
+    @customer_data        = CustomerLoader.new.load_all
+    @merchant_data        = MerchantLoader.new.load_all
+    @invoice_data         = InvoiceLoader.new.load_all
+    @item_data            = ItemLoader.new.load_all
+    @invoice_item_data     = InvoiceItemLoader.new.load_all
+    @transaction_data     = TransactionLoader.new.load_all
   end
 
   def start
-    @customer_repo = CustomerRepo.new(@customer_data, self)
-    @merchant_repo = MerchantRepo.new(@merchant_data, self)
-    @invoice_repo = InvoiceRepo.new(@invoice_data, self)
-    @item_repo = ItemRepo.new(@item_data, self)
-    @invoiceitem_repo = InvoiceItemRepo.new(@invoiceitem_data, self)
+    @customer_repo    = CustomerRepo.new(@customer_data, self)
+    @merchant_repo    = MerchantRepo.new(@merchant_data, self)
+    @invoice_repo     = InvoiceRepo.new(@invoice_data, self)
+    @item_repo        = ItemRepo.new(@item_data, self)
+    @invoice_item_repo = InvoiceItemRepo.new(@invoice_item_data, self)
     @transaction_repo = TransactionRepo.new(@transaction_data, self)
   end
 
