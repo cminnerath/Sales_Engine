cf9e0f0530eaa9e77829fae338babceb3e88138e
cminnerath <cminnerath@gmail.com>
2015-08-05 18:28:47 -0600

    Sales_Engine Build Out (pairing)
    build

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -5,14 +5,40 @@ require_relative 'customer_repo'
 class SalesEngine
 
   attr_accessor :customer_repo,
-                :customer_data
+                :customer_data,
+                :merchant_data,
+                :invoice_data,
+                :item_data,
+                :invoiceitem_data,
+                :transaction_data,
+                :customer_repo,
+                :merchant_repo,
+                :invoice_repo,
+                :item_repo,
+                :invoiceitem_repo,
+                :transaction_repo
+
+
+
 
   def initialize
     @customer_data = CustomerLoader.new.load_all
+    @merchant_data = MerchantLoader.new.load_all
+    @invoice_data  = InvoiceLoader.new.load_all
+    @item_data     =     ItemLoader.new.load_all
+    @invoiceitem_data =     InvoiceItemLoader.new.load_all
+    @transaction_data = TransactionLoader.new.load_all
+
+
   end
 
   def start
     @customer_repo = CustomerRepo.new(@customer_data, self)
+    @merchant_repo = MerchantRepo.new(@merchant_data, self)
+    @invoice_repo = InvoiceRepo.new(@invoice_data, self)
+    @item_repo = ItemRepo.new(@item_data, self)
+    @invoiceitem_repo = InvoiceItemRepo.new(@invoiceitem_data, self)
+    @transaction_repo = TransactionRepo.new(@transaction_data, self)
   end
 
 end
