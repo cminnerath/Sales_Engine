6b73cbe4fac78aaae045cea67a2bbcc8f266547e
cminnerath <cminnerath@gmail.com>
2015-08-06 20:06:41 -0600

    invoice_test work
    2 passing tests

--- test/Invoice_test.rb
+++ test/Invoice_test.rb
@@ -5,6 +5,22 @@ require 'minitest/pride'
 require 'csv'
 require "./lib/invoice"
 
-class InvoiceTest < Minitest::test
+class InvoiceTest < Minitest::Test
+
+  def setup
+    file = "./data/invoices_fixture.csv"
+    invoice_csv = CSV.open(file, headers: true, header_converters: :symbol)
+    invoice_collection = invoice_csv.map { |row| Invoice.new(row) }
+  end
+
+  def test_it_can_access_id
+    id = setup[0].id
+    assert_equal 1 , id
+  end
+
+  def test_it_can_access_customer_id
+    customer_id = setup[8].customer_id
+    assert_equal 2, customer_id
+  end
 
 end
