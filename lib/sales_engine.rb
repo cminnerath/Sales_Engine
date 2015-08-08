855fbbf7ba9f8d3273c74f22f343b2bfd10ed36f
Adam Ki Jensen <adajensen@gmail.com>
2015-08-07 23:22:29 -0600

    Update sales_engine.rb

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -13,20 +13,20 @@ class SalesEngine
 
   def initialize
     @customer_data        = CustomerLoader.new.load_all
-    # @merchant_data        = MerchantLoader.new.load_all
-    # @invoice_data         = InvoiceLoader.new.load_all
-    # @item_data            = ItemLoader.new.load_all
-    # @invoice_item_data    = InvoiceItemLoader.new.load_all
-    # @transaction_data     = TransactionLoader.new.load_all
+    @merchant_data        = MerchantLoader.new.load_all
+    @invoice_data         = InvoiceLoader.new.load_all
+    @item_data            = ItemLoader.new.load_all
+    @invoice_item_data    = InvoiceItemLoader.new.load_all
+    @transaction_data     = TransactionLoader.new.load_all
   end
 
   def start
     @customer_repo    = CustomerRepo.new(@customer_data, self)
-    # @merchant_repo    = MerchantRepo.new(@merchant_data, self)
-    # @invoice_repo     = InvoiceRepo.new(@invoice_data, self)
-    # @item_repo        = ItemRepo.new(@item_data, self)
-    # @invoice_item_repo = InvoiceItemRepo.new(@invoice_item_data, self)
-    # @transaction_repo = TransactionRepo.new(@transaction_data, self)
+    @merchant_repo    = MerchantRepo.new(@merchant_data, self)
+    @invoice_repo     = InvoiceRepo.new(@invoice_data, self)
+    @item_repo        = ItemRepo.new(@item_data, self)
+    @invoice_item_repo = InvoiceItemRepo.new(@invoice_item_data, self)
+    @transaction_repo = TransactionRepo.new(@transaction_data, self)
   end
 
 end
