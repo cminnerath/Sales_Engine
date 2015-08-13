e0ec232c8e718b11f539c09eca550136a1d886cd
cminnerath <cminnerath@gmail.com>
2015-08-12 19:56:25 -0600

    best_day_working
    spec passes

--- lib/item.rb
+++ lib/item.rb
@@ -16,11 +16,10 @@ class Item
       @name         = parameters[:name]
       @description  = parameters[:description]
       @unit_price   = BigDecimal.new(parameters[:unit_price])
-      # require "pry";binding.pry
-      @merchant_id   = parameters[:merchant_id].to_i
+      @merchant_id  = parameters[:merchant_id].to_i
       @created_at   = parameters[:created_at]
       @updated_at   = parameters[:updated_at]
-      @repository         = repository
+      @repository   = repository
 
     end
 
@@ -32,7 +31,29 @@ class Item
       repository.find_invoice_items_by_item(id)
     end
 
-
-    def size
+    def find_invoices
+      invoice_items.map do |invoice_item|
+        invoice_item.invoice
+      end
     end
+
+    def invoice_dates
+      find_invoices.map do |invoice|
+        invoice.created_at
+      end
+    end
+
+    def invoice_date_calculator
+      invoice_dates.inject(Hash.new(0)) do |hash, date|
+        hash[date] += 1; hash
+      end
+    end
+
+    def best_day
+      best_day = invoice_date_calculator.max_by do |date, calc|
+        calc
+      end
+      best_day[0]
+    end
+
 end
